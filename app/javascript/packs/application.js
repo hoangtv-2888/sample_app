6e90edee232d91a3f9d5f321189d451339118db9
hoangtv-2888 <tran.viet.hoang@sun-asterisk.com>
2022-01-11 09:02:19 +0700

    Basic login

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,6 @@
+require('bootstrap')
+require('jquery')
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
+require('channels')
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
   root"static_pages#home"
-  get "/signup", to: "users#new"
+
   get "/home", to: "static_pages#home"
   get "/help", to: "static_pages#help"
   get "/contact", to: "static_pages#contact"
   get "/about", to: "static_pages#about"
-  scope "(:locale)", locale: /en|vi/ do
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
+
+  resources :users, only: %i(new create show)
   end
-  resources :users
 end
